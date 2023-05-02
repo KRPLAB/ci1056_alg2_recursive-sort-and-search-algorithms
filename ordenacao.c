9d06cb9b70162388a67bd9d43f78413f96fbdefe
Kauan da Rosa Paulino <55677667+KRP18@users.noreply.github.com>
2023-05-02 12:16:19 -0300

    Update ordenacao.c

--- ordenacao.c
+++ ordenacao.c
@@ -1,5 +1,7 @@
 #include "ordenacao.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
 
 void getNome(char nome[]){
@@ -13,7 +15,7 @@ unsigned int getGRR(){
 	return ;
 }
 
-int bSeq_aux(int vetor[], int a, int b, int x, int* numComparacoes) {
+int bSeq_aux(int vetor[], int a, int b, int x, int* numComparacoes){
     if (a > b)
         return -1;
     if (vetor[b] == x) 
@@ -29,7 +31,7 @@ int buscaSequencial(int vetor[], int tam, int valor, int* numComparacoes){
 	return bSeq_aux(vetor, 0, tam-1, valor, numComparacoes);
 }
 
-int bBin_aux(int vetor[], int a, int b, int x, int* numComparacoes) {
+int bBin_aux(int vetor[], int a, int b, int x, int* numComparacoes){
     if (b >= a) 
 	{
         int m = a + (b - a) / 2;
@@ -48,7 +50,7 @@ int buscaBinaria(int vetor[], int tam, int valor, int* numComparacoes){
 	return bBin_aux(vetor, 0, tam-1, valor, numComparacoes);
 }
 
-void insere(int v[], int a, int b) {
+void insere(int v[], int a, int b){
     int ultimo = v[b-1];
     int j = b-2;
 
@@ -77,19 +79,123 @@ int insertionSort(int vetor[], int tam){
 	return numComparacoes;
 }
 
+// Funcao para trocar dois elementos do vetor
+void troca(int *i, int *j) {
+	int temp = *i;
+	*i = *j;
+	*j = temp;
+}
+
+// Funcao para encontrar o indice do menor elemento do vetor
+int minimo(int vetor[], int a, int b, int *numComparacoes){
+	int menorIndex;
+	
+	if (a == b)
+		return a;
+	
+	(*numComparacoes)++;
+	menorIndex = minimo(vetor, a, b-1, numComparacoes);
+	
+	if (vetor[b] < vetor[menorIndex])
+		menorIndex = b;
+		
+	return menorIndex;
+}
+
+// Funcao para ordenar o vetor usando o algoritmo selection sort
+void selSort_aux(int vetor[], int a, int b, int *numComparacoes){
+    if (a >= b)
+        return;
+	
+	troca(&vetor[a], &vetor[minimo(vetor, a, b, numComparacoes)]);
+	return selSort_aux(vetor, a + 1, b, numComparacoes);
+}
+
 int selectionSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    int numComparacoes = 0;
+	
+    selSort_aux(vetor, 0, tam, &numComparacoes);
+    
+    return numComparacoes;
+}
+
+void intercala(int vetor[], int a, int m, int b){
+	int u[b];
+	int i = a;
+	int j = m + 1;
+
+	if (a >= b)
+		return;
+	
+    for (int k = 0; k <= b-a; k++)
+	{
+		(*numComparacoes)++;
+		if ((j > b) || (i <= m && vetor[i] <= vetor[j]))
+		{
+			u[k] = vetor[i];
+			i++;
+		}
+		else
+		{
+			u[k] = vetor[j];
+			j++;
+		}
+	}
+	memcpy(vetor + a, u, (b - a + 1) * sizeof(int));
+	return;
+}
+
+void mSort_aux(int vetor[], int a, int b, int *numComparacoes){
+    if (a >= b)
+		return;
+		
+	int m = floor((a+b)/2);
+	mSort_aux(vetor, a, m, numComparacoes);
+	mSort_aux(vetor, m+1, b, numComparacoes);
+	
+	return intercala(vetor, a, m, b, numComparacoes);
 }
 
 int mergeSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    int numComparacoes = 0;
+	
+    mSort_aux(vetor, 0, tam, &numComparacoes);
+    
+    return numComparacoes;
+}
+
+int particiona(int vetor[], int a, int b, int *numComparacoes){
+    int x = vetor[b];
+    int i = (a - 1);
+ 
+    for (int j = a ; j <= b-1 ; j++)
+	{
+		(*numComparacoes)++;
+        if (vetor[j] <= x)
+		{
+            i++;
+            troca(&vetor[i], &vetor[j]);
+        }
+    }
+    troca(&vetor[i + 1], &vetor[b]);
+    return (i + 1);
+}
+ 
+void qSort_aux(int vetor[], int a, int b, int *numComparacoes){
+    if (a > b)
+		return;
+		
+	int p = particiona(vetor, a, b, numComparacoes);
+	qSort_aux(vetor, a, p - 1, numComparacoes);
+	qSort_aux(vetor, p + 1, b, numComparacoes);
 }
 
 int quickSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+	int numComparacoes = 0;
+	
+	qSort_aux(vetor, 0, tam, &numComparacoes);
+	
+	return numComparacoes;
 }
 
 int heapSort(int vetor[], int tam){
